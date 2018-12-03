886f613dfd17975964b3675238c7b5d5550cac9e
rg.fakhrutdinov@outlook <6me3qzM4>
2018-12-03 23:34:20 +0300

    comm

---
+++ some.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+import dbf
+
+table = dbf.Table("UER.DBF", codepage='cp866')
+# table.open('read-only')
+table.open()
+
+print(table.field_names)
+
+lt = []
+dt = {}
+
+for i in table:
+    print(i)
+    lt.append(dt)
+
+
+def connect():
+    conn = sqlite3.connect("bank.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS uer_tb (vkey TEXT, "
+                "uer INTEGER, uername TEXT)")
+    conn.commit()
+    for i in table:
+        cur.execute("INSERT INTO uer_tb VALUES(?,?,?,?)", (i['vkey'], i['uer'],
+                                                             i['uername']))
+        conn.commit()
+    conn.close()
+    
+    
+connect()
